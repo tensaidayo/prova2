e268b8e9748664c5f36a94a7b9bf4f2a2042d95b
MatiasAshizawa <ashizawa@hotmail.com>
2019-07-08 14:48:14 -0300

    Edicao nos Categorys e Produtos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,6 +26,7 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
+    @product.user = current_user
 
     respond_to do |format|
       if @product.save
@@ -70,6 +71,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :description, :price)
+      params.require(:product).permit(:title, :description, :price, :category_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :products
+  devise_for :users, :controllers => {sessions: 'users/sessions', registrations: 'users/registrations'}
   resources :categories
+
+  root to: "products#homepage"
+
+  resources :products do
+    collection do
+      get 'homepage'
+      post 'homepage'
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
